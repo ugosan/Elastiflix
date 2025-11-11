b7256b42c1e0c0bb4d800ca967ed2b6474436f35
Ugo Sangiorgi <ugo.sangiorgi@elastic.co>
2025-11-11 11:38:54 -0500

    add instruqt host to vite server config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true
+    host: '0.0.0.0',
+    allowedHosts: ['.play.instruqt.com']
   },
   build: {
     outDir: 'build',
